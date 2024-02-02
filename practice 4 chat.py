4888ca703db33e8b7a7f013022548e76a5d596e6
Катерина Голубенко <109754382+HolubenkoKaterina@users.noreply.github.com>
2024-02-02 14:02:07 +0200

    Add files via upload

---
+++ practice 4 chat.py
@@ -0,0 +1,27 @@
+# Напишите функцию, которая принимает три числа и возвращает максимальное из них.
+# def find_max(a, b, c):
+#     list_num = [a, b, c]
+#     return max(list_num, key=lambda num: num)
+#
+# # Пример использования:
+# result = find_max(15, 177, 20)
+# print(result)  # Ожидаемый вывод: 20
+
+
+# ### Задача: Подсчет гласных букв
+# Напишите функцию, которая принимает строку и возвращает количество гласных букв в этой строке.
+# Гласные буквы: 'a', 'e', 'i', 'o', 'u' (не учитывайте регистр).
+
+# def count_vowels(s):
+#     vowels = 'aeiou'
+#     counter = 0
+#     for letter in s.lower():
+#         if letter in vowels:
+#             counter += 1
+#     return counter
+
+# Пример использования:
+# result = count_vowels("Hello, World!")
+# print(result)  # Ожидаемый вывод: 3
+
+
