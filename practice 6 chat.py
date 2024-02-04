bf7bf1dba5d835bbfcafb0d2a5788abb4bde75c4
Катерина Голубенко <109754382+HolubenkoKaterina@users.noreply.github.com>
2024-02-04 08:06:09 +0200

    Add files via upload

---
+++ practice 6 chat.py
@@ -0,0 +1,27 @@
+### Задача: Сумма цифр числа
+# Напишите функцию, которая принимает целое число и возвращает сумму его цифр.
+# def sum_of_digits(number):
+#     summa = 0
+#     number = str(number)
+#
+#     for num in number:
+#         summa += int(num)
+#     return summa
+# # Пример использования:
+# result1 = sum_of_digits(123)
+# print(result1)  # Ожидаемый вывод: 6
+# result2 = sum_of_digits(4567)
+# print(result2)  # Ожидаемый вывод: 22
+
+### Задача: Поиск максимального элемента
+# Напишите функцию, которая принимает список чисел и возвращает максимальный элемент в этом списке.
+#
+# def find_max_element(nums):
+#     return max(nums, key=lambda elem: elem)
+#
+# # Пример использования:
+# numbers = [3, 18, 1, 6, 2, 8, 5]
+# result = find_max_element(numbers)
+# print(result)  # Ожидаемый вывод: 8
+#
+#
